256aab526044342e954c80e5ca73f293eda3ea9b
Nikhil Rane <nikhilrane1992@gamil.com>
2017-04-29 14:52:37 +0530

    added static files and templates

--- desynova/desynova/urls.py
+++ desynova/desynova/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'desynova.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', views.landing_page),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
