19aa0743062db4f114c89f4cf4a388b53c93b5b2
Nikhil Rane <nikhilrane1992@gamil.com>
2017-04-30 21:58:30 +0530

    added allowed host

--- desynova/desynova/settings.py
+++ desynova/desynova/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rapun%j_m#es4(3)l061-4)e-3cqg#jos1bci2^)qlsz4xa6(l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
